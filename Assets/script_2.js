532a4191a5f35f64996aa261509f835f605f93b3
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-01 20:44:28 -0400

    adding js functions

--- Assets/script_2.js
+++ Assets/script_2.js
@@ -1,8 +1,10 @@
 var btn= document.querySelector("#btn-search")
 var containerToday =document.querySelector("#todayCity")
 var containerForecast=document.querySelector("#forecastCity")
-var dataStore =JSON.parse(localStorage.getItem ("cities"))||[];
-var weather
+var containierSearchHistory=document.querySelector("#searchHistory")
+var storedData =JSON.parse(localStorage.getItem ("cities")) || [];
+
+var weatherCondition=[]
 var urlIcon;
 if(location.protcol === 'http:'){
     urlIcon = "http://openeweathermap.org/img/wn";
@@ -10,3 +12,28 @@ if(location.protcol === 'http:'){
     urlIcon = "https://openweathermap.org/img/wn";
 
 }
+
+//functions for API and code
+
+function start() {
+    loadCity();
+}
+var loadCity = function(){
+
+}
+
+searchClearing(containerSearchHistory)
+    
+if (storedData){
+    var ulEl = document.createElement("ul");
+    ulEl.classList.add("list-unstyled");
+    ulEl.classList.add("w-100");
+
+    for (var i =0; i <storedData.length; i++){
+        
+        var listEl=document.createElement("li");
+        listEl.innerHTML ="<button type='button' class='list-group-item list-group-item-action' attr=' "+storedData[i]+"'>"+storedData[i] + "</button";
+        ulEl.appendChild(listEL);
+    }
+    containierSearchHistory.appendChild(ulEl);
+};
